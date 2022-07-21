f92924d9585b40939160491a120f1910c770310d
Jiayuan LI <abbyjiayuanli@gmail.com>
2022-07-21 14:32:06 -0700

    update index.html

--- src/main/java/Abby/demo/controller/HomeController.java
+++ src/main/java/Abby/demo/controller/HomeController.java
@@ -1,5 +1,12 @@
 package Abby.demo.controller;
 
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import Abby.demo.dao.DiscussPostMapper;
+import Abby.demo.entity.DiscussPost;
+import Abby.demo.entity.User;
 import Abby.demo.service.UserService;
 
 @Controller
@@ -18,7 +27,18 @@ public class HomeController {
 	
 	@RequestMapping(path="/index", method=RequestMethod.GET)
 	public String getIndexPage(Model model) {
-		
+		List<DiscussPost> list = discussPostMapper.selectDisPosts(0, 0, 0);
+		List<Map<String, Object>> discussPosts = new ArrayList<>();
+		if (list!=null) {
+			for (DiscussPost post:list) {
+				Map<String,Object> map = new HashMap<>();
+				map.put("post",post);
+				User user = userService.findById(post.getUserId());
+				map.put("user", user);
+				discussPosts.add(map);
+			}
+		}
+		model.addAttribute("discussPosts", discussPosts);
 		return "/index";
 	}
 	
