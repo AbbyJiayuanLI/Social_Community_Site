66cb2535a341d7fcd59c0e9f3b2c6870b9a39953
Jiayuan LI <abbyjiayuanli@gmail.com>
2022-07-21 13:32:17 -0700

    upload static and template files

---
+++ src/main/java/Abby/demo/service/UserService.java
@@ -0,0 +1,17 @@
+package Abby.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import Abby.demo.dao.UserMapper;
+import Abby.demo.entity.User;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserMapper userMapper;
+	
+	public User findById(int id) {
+		return userMapper.selectById(id);
+	}
+}
