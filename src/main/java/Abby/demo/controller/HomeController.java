3e588786025291d1e83f53ad790cee521f3dcc6e
Jiayuan LI <abbyjiayuanli@gmail.com>
2022-07-21 14:04:06 -0700

    add HomeController for view layer

---
+++ src/main/java/Abby/demo/controller/HomeController.java
@@ -0,0 +1,25 @@
+package Abby.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import Abby.demo.dao.DiscussPostMapper;
+import Abby.demo.service.UserService;
+
+@Controller
+public class HomeController {
+	@Autowired
+	private DiscussPostMapper discussPostMapper;
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(path="/index", method=RequestMethod.GET)
+	public String getIndexPage(Model model) {
+		return "/index";
+		
+	}
+	
+}
