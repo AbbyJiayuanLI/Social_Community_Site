180a50a0db9e3396fa389bc73a52bbf93b2e4126
Jiayuan LI <abbyjiayuanli@gmail.com>
2022-07-23 15:13:18 -0700

    add logger test

--- src/test/java/Abby/demo/LoggerTest.java
+++ src/test/java/Abby/demo/LoggerTest.java
@@ -1,5 +1,6 @@
 package Abby.demo;
 
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,5 +11,15 @@ import org.springframework.test.context.ContextConfiguration;
 public class LoggerTest {
 	
 	private static final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
-
+	
+	@Test
+	public void testLogger() {
+		System.out.println(logger.getName());
+		
+		logger.debug("debug log");
+		logger.info("info log");
+		logger.warn("warn log");
+		logger.error("error log");
+	}
+	
 }
