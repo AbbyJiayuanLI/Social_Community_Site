0097ba7fb461d74e979f5c83da06a1f17c24648a
Jiayuan LI <abbyjiayuanli@gmail.com>
2022-07-24 14:35:36 -0700

    add test html

--- src/test/java/Abby/demo/MailClientTest.java
+++ src/test/java/Abby/demo/MailClientTest.java
@@ -1,11 +1,20 @@
 package Abby.demo;
 
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import Abby.demo.util.MailClient;
+import ch.qos.logback.core.spi.LifeCycle;
+import ch.qos.logback.core.status.StatusManager;
 
 
 @SpringBootTest
@@ -15,10 +24,25 @@ public class MailClientTest {
 	@Autowired
 	MailClient mailClient;
 	
+	@Autowired
+	private TemplateEngine templateEngine;
+	
 	@Test
 	public void testSendMail() {
 		mailClient.sendMail("abbyjiayuanli@gmail.com", "test mailClient", "test mail content");
 		
+	}
+	
+	
+	@Test
+	public void testHtmlMail() {
+		Context context = new Context();
+		context.setVariable("username", "xxxname");
+		
+		String content = templateEngine.process("/mail/demo",context);
+		System.out.println(content);
+		
+		mailClient.sendMail("abbyjiayuanli@gmail.com", "test html", content);
 	}
 
 }
