ef48bfb4ec8a524a2b0ad027d007070718ba8b4d
Jiayuan LI <abbyjiayuanli@gmail.com>
2022-07-24 15:11:47 -0700

    add static util function

---
+++ src/main/java/Abby/demo/util/demoUtil.java
@@ -0,0 +1,26 @@
+package Abby.demo.util;
+
+import java.util.UUID;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.DigestUtils;
+
+public class demoUtil {
+	
+	//generate random str
+	public static String genUUID() {
+		return UUID.randomUUID().toString().replaceAll("-", "");
+	}
+	
+	// MD5 encryption (salt??)
+	public static String md5(String key) {
+		if (StringUtils.isBlank(key)) {
+			return null;
+		} else {
+			return DigestUtils.md5DigestAsHex(key.getBytes());
+		}
+		
+	}
+	
+
+}
