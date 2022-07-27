41b65fce043a334a76818805d4bd07acb9e6b248
Jiayuan LI <abbyjiayuanli@gmail.com>
2022-07-26 17:38:50 -0700

    add expiry related

--- src/main/java/Abby/demo/util/DemoConstant.java
+++ src/main/java/Abby/demo/util/DemoConstant.java
@@ -10,4 +10,10 @@ public interface DemoConstant {
 		
 	// 激活失败
 	int ACTIVATION_FAILURE = 2;
+	
+	// 默认登陆凭证超时时间
+	int DEFAULT_EXPIRE_SEC = 12*60*60;
+	
+	// 记住之后登陆凭证超时时间
+	int REMEMBER_EXPIRE_SEC = 100*12*60*60;
 }
