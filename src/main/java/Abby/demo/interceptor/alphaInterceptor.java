86db4cfeba15cb164f5992ed300667a283823c87
Jiayuan LI <abbyjiayuanli@gmail.com>
2022-07-27 23:57:25 -0700

    add interceptor

---
+++ src/main/java/Abby/demo/interceptor/alphaInterceptor.java
@@ -0,0 +1,9 @@
+package Abby.demo.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class alphaInterceptor implements HandlerInterceptor{
+
+}
