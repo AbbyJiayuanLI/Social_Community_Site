6985f2309d533e29d05ea7907655f42357f381e3
Jiayuan LI <abbyjiayuanli@gmail.com>
2022-08-07 15:42:04 -0700

    add likeSerive

--- src/main/java/Abby/demo/util/RedisKeyUtil.java
+++ src/main/java/Abby/demo/util/RedisKeyUtil.java
@@ -6,7 +6,7 @@ public class RedisKeyUtil {
 	private static final String PREFIX_LIKE = "like:entity";
 	
 	//实体：like:entity:entityType:entityId -> set(userId)
-	public String getEntityLikeKey(int entityType, int entityId) {
+	public static String getEntityLikeKey(int entityType, int entityId) {
 		return PREFIX_LIKE+SPLIT+entityType+SPLIT+entityId;
 	}
 
