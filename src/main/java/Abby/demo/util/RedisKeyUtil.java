37047534d21bb854b518134a2e7135e4a19b889c
Jiayuan LI <abbyjiayuanli@gmail.com>
2022-08-14 16:51:33 -0700

    add getUserLikeKey

--- src/main/java/Abby/demo/util/RedisKeyUtil.java
+++ src/main/java/Abby/demo/util/RedisKeyUtil.java
@@ -5,9 +5,14 @@ public class RedisKeyUtil {
 	private static final String SPLIT = ":";
 	private static final String PREFIX_LIKE = "like:entity";
 	
-	//实体：like:entity:entityType:entityId -> set(userId)
+	//实体的赞：like:entity:entityType:entityId -> set(userId)
 	public static String getEntityLikeKey(int entityType, int entityId) {
 		return PREFIX_LIKE+SPLIT+entityType+SPLIT+entityId;
+	}
+	
+	//个体的赞：like:user:userId -> int
+	public static String getUserLikeKey(int userId) {
+		return PREFIX_LIKE+SPLIT+userId;
 	}
 
 }
