56c63749d35185d30fb5de1813ba517196e7288c
Jiayuan LI <abbyjiayuanli@gmail.com>
2022-08-16 13:54:51 -0700

    add following key

--- src/main/java/Abby/demo/util/RedisKeyUtil.java
+++ src/main/java/Abby/demo/util/RedisKeyUtil.java
@@ -4,6 +4,8 @@ public class RedisKeyUtil {
 	
 	private static final String SPLIT = ":";
 	private static final String PREFIX_LIKE = "like:entity";
+	private static final String PREFIX_FOLLOWER = "follower";
+	private static final String PREFIX_FOLLOWEE = "followee";
 	
 	//实体的赞：like:entity:entityType:entityId -> set(userId)
 	public static String getEntityLikeKey(int entityType, int entityId) {
@@ -13,6 +15,16 @@ public class RedisKeyUtil {
 	//个体的赞：like:user:userId -> int
 	public static String getUserLikeKey(int userId) {
 		return PREFIX_LIKE+SPLIT+userId;
+	}
+	
+	//用户关注的实体：followee:userId:entityType -> zset(entityId, now)
+	public static String getFolloweeKey(int userId, int entityType) {
+		return PREFIX_FOLLOWEE+SPLIT+userId+SPLIT+entityType;
+	}
+	
+	//用户的粉丝：follower:entityType:entityId -> zset(userId, now)
+	public static String getFollowerKey(int entityType, int entityId) {
+		return PREFIX_FOLLOWER+SPLIT+entityType+SPLIT+entityId;
 	}
 
 }
