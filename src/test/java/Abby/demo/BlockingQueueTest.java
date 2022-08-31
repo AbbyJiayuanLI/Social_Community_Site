3875c3dec0f743862c6ad950bc02719c2379d407
Jiayuan LI <abbyjiayuanli@gmail.com>
2022-08-31 12:17:36 -0700

    add blockingqueuetest

---
+++ src/test/java/Abby/demo/BlockingQueueTest.java
@@ -0,0 +1,20 @@
+package Abby.demo;
+
+public class BlockingQueueTest {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
+
+// 线程，需要实现runnable
+class Producer implements Runnable{
+	
+	
+	
+	@Override
+	public void run() {
+		
+	}
+}
