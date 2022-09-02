f45fcb83e23559061f95c35bc98750e61e5dfefc
Jiayuan LI <abbyjiayuanli@gmail.com>
2022-09-02 16:15:00 -0700

    add message related

---
+++ src/main/java/Abby/demo/controller/MessageController.java
@@ -0,0 +1,69 @@
+package Abby.demo.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import Abby.demo.entity.Message;
+import Abby.demo.entity.Page;
+import Abby.demo.entity.User;
+import Abby.demo.service.MessageService;
+import Abby.demo.service.UserService;
+import Abby.demo.util.HostHolder;
+
+@Controller
+public class MessageController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MessageController.class);
+
+    @Autowired
+    private MessageService messageService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
+    
+    // 私信
+    @RequestMapping(path = "/letter/list", method = RequestMethod.GET)
+    public String getLetterList(Model model, Page page) {
+    	User user = hostHolder.getUser();
+    	
+    	page.setLimit(5);
+    	page.setPath("/letter/list");
+    	page.setRows(messageService.findConversationCount(user.getId()));
+    	
+    	List<Message> convList = messageService.findConversations(user.getId(),page.getOffset(), page.getLimit());
+    	List<Map<String, Object>> convs = new ArrayList<Map<String,Object>>();
+    	if (convList != null) {
+    		for (Message msg : convList) {
+    			Map<String,Object> map = new HashMap<String, Object>();
+    			map.put("conversation", msg);
+    			map.put("letterCount", messageService.findLetterCount(msg.getConversationId()));
+    			map.put("unreadCount", messageService.findLetterUnreadCount(user.getId(), msg.getConversationId()));
+    			int targetId = user.getId() == msg.getFromId() ? msg.getToId() : msg.getFromId();
+                map.put("target", userService.findById(targetId));
+                convs.add(map);
+    		}
+    	}
+        model.addAttribute("conversations", convs);
+	    
+	    int letterUnreadCount = messageService.findLetterUnreadCount(user.getId(), null);
+	    model.addAttribute("letterUnreadCount", letterUnreadCount);
+	
+	    return "/site/letter";
+    
+    }
+    
+
+}
--- src/main/java/Abby/demo/dao/MessageMapper.java
+++ src/main/java/Abby/demo/dao/MessageMapper.java
@@ -1,12 +1,38 @@
 package Abby.demo.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import Abby.demo.entity.Message;
 
 @Mapper
 public interface MessageMapper {
 	
 	// 查询当前用户会话列表
-	
+	List<Message> selectConversations(@Param("userId") int userId, @Param("offset") int offset, @Param("limit") int limit);
+
+    int selectConversationCount(@Param("userId") int userId);
+
+    List<Message> selectLetters(@Param("conversationId") String conversationId, @Param("offset") int offset, @Param("limit") int limit);
+
+    int selectLetterCount(@Param("conversationId") String conversationId);
+
+    int selectLetterUnreadCount(@Param("userId") int userId, @Param("conversationId") String conversationId);
+
+    int insertMessage(Message message);
+
+    int updateStatus(@Param("ids") List<Integer> ids, @Param("status") int status);
+
+    Message selectLatesNotice(@Param("userId") int userId, @Param("topic") String topic);
+
+    int selectNoticeCount(@Param("userId") int userId, @Param("topic") String topic);
+
+    int selectNoticeUnreadCount(@Param("userId") int userId, @Param("topic") String topic);
+
+    List<Message> selectNotices(@Param("userId") int userId, @Param("topic") String topic, @Param("offset") int offset, @Param("limit") int limit);
+}
+
 	
 
-}
